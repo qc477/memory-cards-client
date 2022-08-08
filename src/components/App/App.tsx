0099a521f950f46d3e04cb5fd1a406727b8e1fe0
init63 <shandy4wd@mail.ru>
2022-08-08 12:10:32 +0400

    Adds the main page

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from '../../pages/Home';
 import './App.css';
 
 const App: React.FC = () => {
-  return <>Hello World!</>;
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
+  );
 };
 
 export default App;
