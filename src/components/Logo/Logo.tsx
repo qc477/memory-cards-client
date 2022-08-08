dd99270afdde741284bb6ed4d423ef20f9ac3403
init63 <shandy4wd@mail.ru>
2022-08-08 12:28:23 +0400

    Adds the Logo component

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import cl from './Logo.module.css';
+
+const Logo: React.FC = () => {
+  return <h1 className={cl.logo}>LeitnerSystemApp</h1>;
+};
+
+export default Logo;
