96ab7fad6a52693aec659ea42c458c72537862b0
init63 <shandy4wd@mail.ru>
2022-08-12 11:33:28 +0400

    Rewrites the Navigation component

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { AddButton, InfoButton, SettingsButton } from '../UI/Buttons';
 import cl from './Navigation.module.css';
 
 const Navigation: React.FC = () => {
   return (
-    <div className={cl.headerButtons}>
-      <AddButton />
-      <InfoButton />
-      <SettingsButton />
-    </div>
+    <nav className={cl.navigation}>
+      <ul className={cl.navigationList}>
+        <li className={cl.navigationItem}></li>
+        <li className={cl.navigationItem}></li>
+        <li className={cl.navigationItem}></li>
+      </ul>
+    </nav>
   );
 };
 
