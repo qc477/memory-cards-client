23639095e28f23e52c20ceccec27afbde7ed93bb
init63 <shandy4wd@mail.ru>
2022-08-15 12:57:44 +0400

    Adds a new SearchField component

--- src/components/SortingBlock/SortingBlock.tsx
+++ src/components/SortingBlock/SortingBlock.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import SearchField from '../UI/SearchField';
 import cl from './SortingBlock.module.css';
 
 const SortingBlock: React.FC = () => {
   return (
     <div className={cl.sortingBlock}>
-      SearchField
+      <SearchField />
     </div>
   );
 };
---
+++ src/components/UI/SearchField/SearchField.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import Icons from '../../Icons';
+import cl from './SearchField.module.css';
+
+const SearchField: React.FC = () => {
+  const [isFocuse, setIsFocuse] = useState<boolean>(false);
+  const [value, setValue] = useState<string>('');
+
+  return (
+    <div className={`${cl.searchField} ${isFocuse ? cl.searchFieldFocuse : ''}`}>
+      <Icons className={cl.searchFieldIcon} name='search' width='28' height='28' />
+      <input
+        className={cl.searchFieldInput}
+        type='text'
+        placeholder='Поиск...'
+        onChange={(event) => setValue(event.target.value)}
+        onFocus={() => setIsFocuse(true)}
+        onBlur={() => setIsFocuse(false)}
+      />
+    </div>
+  );
+};
+
+export default SearchField;
