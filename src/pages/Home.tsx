45d1f806e4bd885cf07f62eec9f77e7a4d49dd41
init63 <shandy4wd@mail.ru>
2022-08-15 11:27:00 +0400

    Adds styles to pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import SortingBlock from '../components/SortingBlock';
+import cl from './pages.module.css';
 
 const Home: React.FC = () => {
   return (
-    <main>
+    <main className={cl.main}>
       <SortingBlock />
     </main>
   );
