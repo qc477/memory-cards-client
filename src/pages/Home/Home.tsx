54cb3a9633c7d3caa4139560e8ab18cbea6db5dd
init63 <shandy4wd@mail.ru>
2022-08-19 16:40:33 +0400

    Adds a new Main component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import AppContainer from '../../components/AppContainer/AppContainer';
+import Main from '../../components/Main';
+import AppContainer from '../../components/AppContainer';
 import SortingBlock from '../../components/SortingBlock';
 import cl from './Home.module.css';
 
 const Home: React.FC = () => {
   return (
-    <main>
+    <Main>
       <AppContainer>
         <div className={cl.wrapper}>
           <SortingBlock />
         </div>
       </AppContainer>
-    </main>
+    </Main>
   );
 };
 
