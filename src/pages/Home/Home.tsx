5c0ebe95c3d3eca01aaa402a82938c5607cb2379
init63 <shandy4wd@mail.ru>
2022-08-19 16:50:31 +0400

    Edits the layout of the Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,15 +2,12 @@ import React from 'react';
 import Main from '../../components/Main';
 import AppContainer from '../../components/AppContainer';
 import SortingBlock from '../../components/SortingBlock';
-import cl from './Home.module.css';
 
 const Home: React.FC = () => {
   return (
     <Main>
       <AppContainer>
-        <div className={cl.wrapper}>
-          <SortingBlock />
-        </div>
+        <SortingBlock />
       </AppContainer>
     </Main>
   );
