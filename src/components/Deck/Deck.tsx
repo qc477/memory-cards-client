7c18ed6dbad9050757f45b1312498adfb1b2eb56
init63 <shandy4wd@mail.ru>
2022-08-30 12:40:54 +0400

    Small changes

--- src/components/Deck/Deck.tsx
+++ src/components/Deck/Deck.tsx
@@ -14,13 +14,13 @@ const Deck: React.FC = () => {
           <p className={cl.infoText}>Всего слов: 10000</p>
           <p className={cl.infoText}>Упражнений: 23</p>
         </div>
-        <ul className={cl.statistics}>
-          <li className={cl.statisticsItem}>A: 9K</li>
-          <li className={cl.statisticsItem}>B: 450</li>
-          <li className={cl.statisticsItem}>C: 200</li>
-          <li className={cl.statisticsItem}>D: 250</li>
-          <li className={cl.statisticsItem}>E: 100</li>
-        </ul>
+        <ol className={cl.statistics} type='A'>
+          <li className={cl.statisticsItem}>9K</li>
+          <li className={cl.statisticsItem}>450</li>
+          <li className={cl.statisticsItem}>200</li>
+          <li className={cl.statisticsItem}>250</li>
+          <li className={cl.statisticsItem}>100</li>
+        </ol>
       </div>
     </article>
   );
