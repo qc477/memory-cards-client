6a02433132b85cde934efe4b075167f34bb9d2ea
init63 <shandy4wd@mail.ru>
2022-09-09 19:32:55 +0400

    Binds DecksService to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,18 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import headerReducer from './reducers/HeaderSlice';
 import logoReducer from './reducers/LogoSlice';
+import { decksAPI } from '../services/DecksService';
 
 const rootReduser = combineReducers({
   headerReducer,
-  logoReducer
+  logoReducer,
+  [decksAPI.reducerPath]: decksAPI.reducer,
 });
 
 export const setupStore = () => {
   return configureStore({
     reducer: rootReduser,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(decksAPI.middleware),
   });
 };
 
