01bcadc088d5033a3008d39f803ccb00b9d177c1
init63 <shandy4wd@mail.ru>
2022-09-13 20:54:37 +0400

    Configures lazy page loading

--- src/components/AppRoutes/AppRoutes.tsx
+++ src/components/AppRoutes/AppRoutes.tsx
@@ -1,13 +1,37 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { About, Home, Settings } from '../../pages';
+
+const Home = React.lazy(() => import('../../pages/Home'));
+const About = React.lazy(() => import('../../pages/About'));
+const Settings = React.lazy(() => import('../../pages/Settings'));
 
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='about' element={<About />} />
-      <Route path='settings' element={<Settings />} />
+      <Route
+        path='/'
+        element={
+          <React.Suspense fallback={<p>Загрузка страницы</p>}>
+            <Home />
+          </React.Suspense>
+        }
+      />
+      <Route
+        path='about'
+        element={
+          <React.Suspense fallback={<p>Загрузка страницы</p>}>
+            <About />
+          </React.Suspense>
+        }
+      />
+      <Route
+        path='settings'
+        element={
+          <React.Suspense fallback={<p>Загрузка страницы</p>}>
+            <Settings />
+          </React.Suspense>
+        }
+      />
     </Routes>
   );
 };
--- src/pages/index.tsx
+++
@@ -1,4 +0,0 @@
-import Home from './Home';
-import About from './About';
-import Settings from './Settings';
-export { Home, About, Settings };
