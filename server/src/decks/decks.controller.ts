5174700004140ea68bb5c075cbe0da686413f0f4
init63 <shandy4wd@mail.ru>
2022-09-27 18:51:05 +0400

    Настройка контроллера "deck"

--- server/src/decks/decks.controller.ts
+++ server/src/decks/decks.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { DecksService } from './decks.service';
+import { CreateDeckDto } from './dto/create-deck.dto';
 
 @Controller('decks')
-export class DecksController {}
+export class DecksController {
+  constructor(private deckService: DecksService) {}
+
+  @Post()
+  create(@Body() deckDto: CreateDeckDto) {
+    return this.deckService.createDeck(deckDto);
+  }
+
+  @Get()
+  getAll() {
+    return this.deckService.getAllDecks();
+  }
+}
