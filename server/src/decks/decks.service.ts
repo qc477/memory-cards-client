7d19b303a80138388f7d3ec92deb37cc000d6d91
init63 <shandy4wd@mail.ru>
2022-09-27 18:41:00 +0400

    Начальная настройка сервиса "deck"

--- server/src/decks/decks.service.ts
+++ server/src/decks/decks.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Deck } from './decks.models';
+import { CreateDeckDto } from './dto/create-deck.dto';
 
 @Injectable()
-export class DecksService {}
+export class DecksService {
+  constructor(@InjectModel(Deck) private deckRepository: typeof Deck) {}
+
+  async createDeck(dto: CreateDeckDto) {
+    const deck = await this.deckRepository.create(dto);
+    return deck;
+  }
+
+  async getAllDecks() {
+    const decks = await this.deckRepository.findAll();
+    return decks;
+  }
+}
