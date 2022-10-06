e2609551e14c07368fef5af5f0b20c05779e6ae3
init63 <shandy4wd@mail.ru>
2022-10-06 19:49:13 +0400

    Добавляет кнопку MenuButton

--- client/src/components/UI/Buttons/index.tsx
+++ client/src/components/UI/Buttons/index.tsx
@@ -1,7 +1,8 @@
+import MenuButton from './MenuButton';
 import AddButton from './AddButton';
 import AboutButton from './AboutButton';
 import SettingsButton from './SettingsButton';
 import BackButton from './BackButton';
 import MoreButton from './MoreButton';
 import PlayButton from './PlayButton';
-export { AddButton, AboutButton, SettingsButton, BackButton, MoreButton, PlayButton };
+export { MenuButton, AddButton, AboutButton, SettingsButton, BackButton, MoreButton, PlayButton };
