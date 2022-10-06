74004bd5596f2459f3abf92ab290c243d52089a6
init63 <shandy4wd@mail.ru>
2022-10-06 20:05:05 +0400

    Изменение структуры компонента Header

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import cl from './Header.module.css';
+import { AddButton, MenuButton } from '../UI/Buttons';
 
 interface HeaderProps {
   children: React.ReactNode;
 }
 
 const Header: React.FC<HeaderProps> = ({ children }) => {
-
   return (
     <header className={cl.header}>
       <div className={cl.body}>
-        <div className={cl.childrenBlock}>
-          {children}
-        </div>
+        <MenuButton />
+        {children}
+        <AddButton />
       </div>
     </header>
   );
