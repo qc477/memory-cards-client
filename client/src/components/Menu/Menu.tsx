2a66b288a09977df0ade3648d81cd64e2fccbe0e
qc477 <shandy4wd@mail.ru>
2022-10-07 14:15:34 +0400

    Initial configuration of the component

--- client/src/components/Menu/Menu.tsx
+++ client/src/components/Menu/Menu.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import './Menu.module.css';
+import { useAppSelector } from 'hooks/redux';
+import cl from './Menu.module.css';
 
 const Menu: React.FC = () => {
-  return <></>;
+  const {isOpen} = useAppSelector(state => state.menuReducer)
+
+  return (
+    <nav className={isOpen ? cl.menuOpen : cl.menuClose}>
+      <div className={cl.menuHeader}>
+        <p className={cl.menuTitle}>Меню</p>
+      </div>
+      <ul className={cl.menuList}>
+        <li className={cl.menuItem}>account</li>
+        <li className={cl.menuItem}>settings</li>
+        <li className={cl.menuItem}>about</li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Menu;
