2a666bea3ec7f27529bd2ffe0c1d15e3a89699f6
qc477 <shandy4wd@mail.ru>
2022-10-07 13:58:32 +0400

    Create slice

---
+++ client/src/store/reducers/MenuSlice.ts
@@ -0,0 +1,21 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface MenuState {
+  isOpen: boolean;
+}
+
+const initialState: MenuState = {
+  isOpen: false,
+};
+
+export const menuSlice = createSlice({
+  name: 'menu',
+  initialState,
+  reducers: {
+    toggleMenu(state, action: PayloadAction<boolean>) {
+      state.isOpen = action.payload;
+    },
+  },
+});
+
+export default menuSlice.reducer;
