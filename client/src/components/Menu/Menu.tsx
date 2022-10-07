8fde782adce79456f0084c48e6ecfa9cf1aaa104
qc477 <shandy4wd@mail.ru>
2022-10-07 14:43:46 +0400

    Update component and styles

--- client/src/components/Menu/Menu.tsx
+++ client/src/components/Menu/Menu.tsx
@@ -3,19 +3,23 @@ import { useAppSelector } from 'hooks/redux';
 import cl from './Menu.module.css';
 
 const Menu: React.FC = () => {
-  const {isOpen} = useAppSelector(state => state.menuReducer)
+  const { isOpen } = useAppSelector((state) => state.menuReducer);
 
   return (
-    <nav className={isOpen ? cl.menuOpen : cl.menuClose}>
-      <div className={cl.menuHeader}>
-        <p className={cl.menuTitle}>Меню</p>
+    <div className={isOpen ? cl.menuOpen : cl.menuClose}>
+      <div className={cl.menuWrapper}>
+        <nav className={cl.menu}>
+          <div className={cl.menuHeader}>
+            <p className={cl.menuTitle}>Меню</p>
+          </div>
+          <ul className={cl.menuList}>
+            <li className={cl.menuItem}>account</li>
+            <li className={cl.menuItem}>settings</li>
+            <li className={cl.menuItem}>about</li>
+          </ul>
+        </nav>
       </div>
-      <ul className={cl.menuList}>
-        <li className={cl.menuItem}>account</li>
-        <li className={cl.menuItem}>settings</li>
-        <li className={cl.menuItem}>about</li>
-      </ul>
-    </nav>
+    </div>
   );
 };
 
