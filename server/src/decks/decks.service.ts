2db7c926d9602d0e2e803a3095b408d1172dc2f2
qc477 <shandy4wd@mail.ru>
2022-10-14 21:10:49 +0400

    edit decks.service

--- server/src/decks/decks.service.ts
+++ server/src/decks/decks.service.ts
@@ -7,49 +7,49 @@ import { CreateCardDto } from './dto/create-card.dto';
 
 @Injectable()
 export class DecksService {
-    constructor(
-        @InjectModel(Deck) private deckRepository: typeof Deck,
-        @InjectModel(Card) private cardRepository: typeof Card,
-    ) { }
+  constructor(
+    @InjectModel(Deck) private deckRepository: typeof Deck,
+    @InjectModel(Card) private cardRepository: typeof Card,
+  ) {}
 
-    /* Decks */
+  /* Decks */
 
-    async createDeck(dto: CreateDeckDto): Promise<Deck> {
-        const deck = await this.deckRepository.create(dto);
-        return deck;
-    }
+  async createDeck(dto: CreateDeckDto): Promise<Deck> {
+    const deck = await this.deckRepository.create(dto);
+    return deck;
+  }
 
-    async getAllDecks(): Promise<Deck[]> {
-        const decks = await this.deckRepository.findAll();
-        return decks;
-    }
+  async getAllDecks(): Promise<Deck[]> {
+    const decks = await this.deckRepository.findAll();
+    return decks;
+  }
 
-    async deleteDeck(id: number): Promise<number> {
-        await this.deckRepository.destroy({ where: { id: id }, force: true });
-        return id;
-    }
+  async deleteDeck(id: number): Promise<number> {
+    await this.deckRepository.destroy({ where: { id: id }, force: true });
+    return id;
+  }
 
-    /* Cards */
+  /* Cards */
 
-    async createCard(dto: CreateCardDto): Promise<Card> {
-        const card = await this.cardRepository.create(dto);
-        const deck = await this.deckRepository.findOne({
-            where: { id: dto.deckId },
-        });
-        deck.totalCards = deck.totalCards + 1;
-        deck.save();
-        return card;
-    }
+  async createCard(dto: CreateCardDto): Promise<Card> {
+    const card = await this.cardRepository.create(dto);
+    const deck = await this.deckRepository.findOne({
+      where: { id: dto.deckId },
+    });
+    deck.totalCards = deck.totalCards + 1;
+    deck.save();
+    return card;
+  }
 
-    async getAllCards(deckId: number): Promise<Card[]> {
-        const cards = await this.cardRepository.findAll({
-            where: { deckId: deckId },
-        });
-        return cards;
-    }
+  async getAllCards(deckId: number): Promise<Card[]> {
+    const cards = await this.cardRepository.findAll({
+      where: { deckId: deckId },
+    });
+    return cards;
+  }
 
-    async deleteCard(id: number): Promise<number> {
-        await this.cardRepository.destroy({ where: { id: id }, force: true });
-        return id;
-    }
+  async deleteCard(id: number): Promise<number> {
+    await this.cardRepository.destroy({ where: { id: id }, force: true });
+    return id;
+  }
 }
