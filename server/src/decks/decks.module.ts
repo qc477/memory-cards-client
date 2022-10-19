c43a509f891e7a417389ceffffdbe358a2b2214a
qc477 <shandy4wd@mail.ru>
2022-10-19 14:38:18 +0400

    small edit

--- server/src/decks/decks.module.ts
+++ server/src/decks/decks.module.ts
@@ -3,11 +3,10 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { DecksController } from './decks.controller';
 import { DecksService } from './decks.service';
 import { Deck } from './model/decks.model';
-import { Card } from '../cards/model/cards.model';
 
 @Module({
   controllers: [DecksController],
   providers: [DecksService],
-  imports: [SequelizeModule.forFeature([Deck, Card])],
+  imports: [SequelizeModule.forFeature([Deck])],
 })
 export class DecksModule {}
