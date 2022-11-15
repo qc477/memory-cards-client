7813199fda2101ed879a1690468a1adf4ce07d7d
qc477 <shandy4wd@mail.ru>
2022-11-15 09:46:57 +0400

    edit IconButton

--- src/components/UI/IconButton/IconButton.tsx
+++ src/components/UI/IconButton/IconButton.tsx
@@ -1,45 +1,27 @@
+import clsx from 'clsx';
 import React from 'react';
 import cl from './IconButton.module.css';
 
-enum Colors {
-  FAINT_STRONG = 'faintStrong',
-  FAINT_STRONG_DOWN = 'faintStrongDown',
-  FAINT_STRONG_TEXT_DOWN = 'faintStrongTextDown',
-}
+type Color = 'faintStrong' | 'faintStrongDown' | 'faintStrongTextDown';
 
 interface IconButtonProps {
   children: React.ReactNode;
-  color?: string;
+  color?: Color;
   _onClick?: () => void;
 }
 
 const IconButton: React.FC<IconButtonProps> = ({ children, color, _onClick }) => {
-  switch (color) {
-    case Colors.FAINT_STRONG:
-      return (
-        <button onClick={_onClick} className={`${cl.baseStyles} ${cl.faintStrong}`}>
-          {children}
-        </button>
-      );
-    case Colors.FAINT_STRONG_DOWN:
-      return (
-        <button onClick={_onClick} className={`${cl.baseStyles} ${cl.faintStrongDown}`}>
-          {children}
-        </button>
-      );
-    case Colors.FAINT_STRONG_TEXT_DOWN:
-      return (
-        <button onClick={_onClick} className={`${cl.baseStyles} ${cl.faintStrongTextDown}`}>
-          {children}
-        </button>
-      );
-    default:
-      return (
-        <button onClick={_onClick} className={cl.baseStyles}>
-          {children}
-        </button>
-      );
-  }
+  const styles = clsx(cl.baseStyles, {
+    [cl.faintStrong]: color === 'faintStrong',
+    [cl.faintStrongDown]: color === 'faintStrongDown',
+    [cl.faintStrongTextDown]: color === 'faintStrongTextDown',
+  });
+
+  return (
+    <button className={styles} onClick={_onClick}>
+      {children}
+    </button>
+  );
 };
 
 export default IconButton;
