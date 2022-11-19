e8ea666c95af2fbe14e21e0df3267c9eb7cbd22d
qc477 <shandy4wd@mail.ru>
2022-11-19 07:45:03 +0400

    add: argument className

--- src/components/UI/Card/Card.tsx
+++ src/components/UI/Card/Card.tsx
@@ -1,12 +1,16 @@
+import clsx from 'clsx';
 import React from 'react';
 import cl from './Card.module.css';
 
 interface CardProps {
   children: React.ReactNode;
+  className?: string;
 }
 
-const Card: React.FC<CardProps> = ({ children }) => {
-  return <article className={cl.card}>{children}</article>;
+const Card: React.FC<CardProps> = ({ children, className }) => {
+  const styles = clsx([cl.card, className]);
+
+  return <article className={styles}>{children}</article>;
 };
 
 export default Card;
